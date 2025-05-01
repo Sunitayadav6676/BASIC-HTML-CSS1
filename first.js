9310560ea40931dbadc4e15dce85957e768cb1ea
Sunitayadav6676 <156333336+Sunitayadav6676@users.noreply.github.com>
2025-05-01 13:09:41 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,74 @@
+// declare variable
+let name="XYZ";
+const vill="Kundoor";
+var district="Kushinagar";
+console.log(name);
+console.log(vill);
+console.log(district);
+
+
+// declear a number boolean string
+let mystring="mmit";
+let mynumber="959595";
+let boolean=true;
+console.log(mystring);
+console.log(mynumber);
+console.log(boolean); 
+
+
+//check a number even or odd
+var number=8;
+if(number%2===0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+
+//print the no. 1 to 10 by using for loop
+
+for (let i=1; i<=10; i++){
+    console.log(i);
+
+}
+
+
+//print the no. 1 to 10 by using while loop
+while(1<=10){
+    i++;
+    console.log(i);
+}
+
+//lower case and upper case
+
+let san="bajarangi";{
+    console.log(san.toLocaleUpperCase());
+}
+
+let asn="MANNI";{
+    console.log(asn.toLocaleUpperCase());
+}
+
+let name = 'sunita '
+let age = 18;
+let address="Kundoor";
+//  by using template literal:
+let msg = `my name is ${name} and i am ${age} year old my district name ${address}`;
+console.log(msg);
+// using charAt 
+console.log(address.charAt(5))
+console.log(Number.parseInt(age));
+console.log(Number.parseFloat(age))
+//array
+var array=["apple","banana","blackberry","mango","lemon"]
+console.log(array);
+array.push("gaaaaaa");
+array.push("hen");
+array.pop("")
+
+
+
+
+
+
+ 
